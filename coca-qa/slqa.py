c01ccfe29bf8b5554faf314eda118f1feb73e318
SparkJiao <jiaofangkai@hotmail.com>
2018-10-23 17:33:53 +0800

    update 10.23

--- coca-qa/slqa.py
+++ coca-qa/slqa.py
@@ -34,8 +34,10 @@ class MultiGranularityHierarchicalAttentionFusionNetworks(Model):
         self._linear_activate = torch.nn.ReLU()
         self._softmax_d1 = torch.nn.Softmax(dim=1)
         self._softmax_d2 = torch.nn.Softmax(dim=2)
-        self._fuse_linear = torch.nn.Linear(in_features=4 * self._encoding_dim, out_features=4 * self._encoding_dim)
+        self._fuse_linear_m = torch.nn.Linear(in_features=4 * self._encoding_dim, out_features=self._encoding_dim)
+        self._fuse_linear_g = torch.nn.Linear(in_features=4 * self._encoding_dim, out_features=self._encoding_dim)
         self._fuse_tanh = torch.nn.Tanh()
+        self._fuse_sigmoid = torch.nn.Sigmoid()
 
         self._span_start_accuracy = CategoricalAccuracy()
         self._span_end_accuracy = CategoricalAccuracy()
@@ -70,3 +72,11 @@ class MultiGranularityHierarchicalAttentionFusionNetworks(Model):
         p_ = torch.mm(self._softmax_d2(s), u_p)
         p_q_ = torch.cat((u_p, q_, u_p * q_, u_p - q_), 2)
         q_p_ = torch.cat((u_q, p_, u_q * p_, u_q - p_), 2)
+        # Shape(batch_size, passage_length, passage_length)
+        pp = torch.mm(self._fuse_sigmoid(self._fuse_linear_g(p_q_)),
+                      self._fuse_tanh(self._fuse_linear_m(p_q_)).permute(0, 2, 1)) + torch.mm(
+            (torch.Tensor([1]) - self._fuse_sigmoid(self._fuse_linear_g(p_q_))), u_p.permute(0, 2, 1))
+        # Shape(batch_size, question_length, question_length)
+        qq = torch.mm(self._fuse_sigmoid(self._fuse_linear_g(q_p_)),
+                      self._fuse_tanh(self._fuse_linear_m(q_p_)).permute(0, 2, 1)) + torch.mm(
+            (torch.Tensor([1]) - self._fuse_sigmoid(self._fuse_linear_g(q_p_))), u_q.permute(0, 2, 1))
